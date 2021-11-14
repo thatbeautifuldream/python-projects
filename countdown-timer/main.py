6c875d622738cf8c1b90573de836bdc407213cc3
Milind Mishra <milind.mishra4@gmail.com>
2021-11-14 15:45:47 +0530

    timer

---
+++ countdown-timer/main.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+    print('\nTime is up!')
+
+t = int(input('Enter time in seconds: '))
+
+countdown(int(t))
